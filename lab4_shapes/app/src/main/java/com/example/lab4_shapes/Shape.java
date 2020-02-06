558a83a3f74f0643e2b7d7cc0969f65191d20907
jha8 <Joeha909@gmail.com>
2020-02-05 19:25:22 -0700

    Edit shape class with green

--- lab4_shapes/app/src/main/java/com/example/lab4_shapes/Shape.java
+++ lab4_shapes/app/src/main/java/com/example/lab4_shapes/Shape.java
@@ -3,6 +3,7 @@ package com.example.lab4_shapes;
 public abstract class Shape {
     public int x;
     public int y;
+    public String color = "green";
     Shape(int x, int y){
         this.x = x;
         this.y = y;
