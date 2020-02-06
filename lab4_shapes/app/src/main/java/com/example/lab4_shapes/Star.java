653743cc9015b67111d5b86bbc595bd1f09a720a
jha8 <Joeha909@gmail.com>
2020-02-05 18:58:34 -0700

    Add Star class

--- lab4_shapes/app/src/main/java/com/example/lab4_shapes/Star.java
+++ lab4_shapes/app/src/main/java/com/example/lab4_shapes/Star.java
@@ -1,5 +1,9 @@
 package com.example.lab4_shapes;
 
 public class Star extends Shape {
-    
+    private final int x, y;
+    public Star(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
 }
