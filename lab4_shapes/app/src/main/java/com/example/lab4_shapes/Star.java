378c40ef46f55ad3046f881c48fb3c95e5de15ab
nmrchvz <46454559+nmrchvz@users.noreply.github.com>
2020-02-05 19:07:24 -0700

    Updated

--- lab4_shapes/app/src/main/java/com/example/lab4_shapes/Star.java
+++ lab4_shapes/app/src/main/java/com/example/lab4_shapes/Star.java
@@ -1,7 +1,6 @@
 package com.example.lab4_shapes;
 
 public class Star extends Shape {
-<<<<<<< HEAD
     @Override
     public int x() {
         return 0;
@@ -10,11 +9,4 @@ public class Star extends Shape {
     @Override
     public int y() {
         return 0;
-=======
-    private final int x, y;
-    public Star(int x, int y){
-        this.x = x;
-        this.y = y;
->>>>>>> 5eca594c2263d68af5d4093c99de92d1431d8534
-    }
 }
