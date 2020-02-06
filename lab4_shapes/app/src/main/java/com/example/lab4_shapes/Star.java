a0f44e4bb4a19b78fb70405eacf8508af80d8b28
nmrchvz <46454559+nmrchvz@users.noreply.github.com>
2020-02-05 19:05:06 -0700

    Updated Star.java

--- lab4_shapes/app/src/main/java/com/example/lab4_shapes/Star.java
+++ lab4_shapes/app/src/main/java/com/example/lab4_shapes/Star.java
@@ -1,5 +1,13 @@
 package com.example.lab4_shapes;
 
 public class Star extends Shape {
-    
+    @Override
+    public int x() {
+        return 0;
+    }
+
+    @Override
+    public int y() {
+        return 0;
+    }
 }
