90c3261c4ef8b9148e532e7b4681811609368268
Nick Anderson <47461646+nick98anderson@users.noreply.github.com>
2020-02-06 15:08:30 -0700

    added trapezoid

---
+++ lab4_shapes/app/src/main/java/com/example/lab4_shapes/Trapezoid.java
@@ -0,0 +1,13 @@
+package com.example.lab4_shapes;
+
+public class Trapezoid extends Shape {
+    @Override
+    public int x() {
+        return 0;
+    }
+
+    @Override
+    public int y() {
+        return 0;
+    }
+}
