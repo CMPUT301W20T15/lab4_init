30d4f1a8552abafb6d2dd03dc90d73d65c1c3249
nmrchvz <46454559+nmrchvz@users.noreply.github.com>
2020-02-05 18:42:57 -0700

    Update

--- lab4_shapes/app/src/main/java/com/example/lab4_shapes/Shape.java
+++ lab4_shapes/app/src/main/java/com/example/lab4_shapes/Shape.java
@@ -1,8 +1,8 @@
 package com.example.lab4_shapes;
 
 public abstract class Shape {
-    private int x;
-    private  int y;
+    public int x;
+    public int y;
     Shape (int x, int y){
         this.x = x;
         this.y = y;
