1b9e46fa5edba3a576124934d7ae0973453d454f
anasiv <anasiv350@gmail.com>
2020-02-05 19:17:45 -0700

    add oval

---
+++ lab4_shapes/app/src/main/java/com/example/lab4_shapes/Oval.java
@@ -0,0 +1,13 @@
+package com.example.lab4_shapes;
+
+public class Oval extends Shape {
+    @Override
+    public int x() {
+        return 0;
+    }
+
+    @Override
+    public int y() {
+        return 0;
+    }
+}
