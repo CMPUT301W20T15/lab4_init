845cc5a93d2324eaac38eb398893af2e8bde88c8
anasiv <anasiv350@gmail.com>
2020-02-07 11:17:41 -0700

    resolving the conflict

--- lab4_shapes/app/src/main/java/com/example/lab4_shapes/Shape.java
+++ lab4_shapes/app/src/main/java/com/example/lab4_shapes/Shape.java
@@ -3,7 +3,7 @@ package com.example.lab4_shapes;
 public abstract class Shape {
     public int x;
     public int y;
-    String color = "Pink";
+    String color = "Orange";
     Shape(int x, int y){
         this.x = x;
         this.y = y;
